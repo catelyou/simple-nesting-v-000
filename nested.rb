a849e502bbc0e55c9a6ba891832bde5aedab8eda
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-23 16:11:27 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,8 @@ def dennis_ritchies_language
         }
      }
 
-	return "#{programmer_hash[:dennis_ritchie][:languages]}"
+	dennis_language = programmer_hash[:dennis_ritchie][:languages]
+	dennis_language.join
 
 end
 
